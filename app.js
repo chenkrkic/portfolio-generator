c14fdace0e02d4f10f6154a06a2208f9c1c7b9c8
jiajie chen <ga-kit@hotmail.com>
2021-07-04 19:25:50 -0700

    generate HTML file

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const [name, github] = profileDataArgs;
+const fs = require('fs');
+const generatePage = require('./src/page-templete.js');
 
-const printProfileData = (profileDataArr) => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
 
-    console.log('=============');
 
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
+  
